169ad5c7c84b47ecacc8fbdd734c6cabe43d13e4
Izunia <iza.obrebska@wp.pl>
2019-02-03 20:04:59 +0100

    bity

---
+++ Bity_włączone.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int l;
+int wyn;
+int bity[8];
+int x = 7;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	cout << "Podaj liczbę: ";
+	cin >> l;
+
+
+	do {
+		wyn = l % 2;
+		bity[x] = wyn;
+		l = l / 2;
+		x--;
+	} while (l != 0);
+	cout << "\nLiczba w zapisie binarnym:";
+	for (x = 0; x < 8; x++)
+	{
+		cout << bity[x];
+	}
+	cout << endl;
+	cout << "\nBity włączone: \n";
+	for (x = 7; x >= 0; x--)
+	{
+		if (bity[x] == 1) cout << "\nWłączono bit nr: " << x << ".";
+	}
+	cout << endl;
+	cout << endl;
+	system("pause");
+	return 0;
+}
---
+++ bity_Losuj_Sprawdz.cpp
@@ -0,0 +1,54 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int liczba;
+int wyn;
+int bity[8];
+int bitylos[8];
+int x = 7;
+int bit;
+int ilerazywylosowano;
+float srednia;
+
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "");
+	cout << "Podaj liczbę: ";
+	cin >> liczba;
+	do {
+		wyn = liczba % 2;
+		bity[x] = wyn;
+		liczba = liczba / 2;
+		x--;
+	} while (liczba != 0);
+	cout << "Wybierz numer bitu: ";
+	cin >> bit;
+	cout << "Zawartość wybranego bitu: " << bity[bit] << endl;
+	for (int y = 0; y < 10; y++)
+	{
+		cout << "\nLosuję liczbę ...";
+		int liczbawylosowana = (std::rand() % 256) + 0;
+		cout << liczbawylosowana;
+		x = 7;
+		do {
+			wyn = liczbawylosowana % 2;
+			bity[x] = wyn;
+			liczbawylosowana = liczbawylosowana / 2;
+			x--;
+		} while (liczbawylosowana != 0);
+		if (bity[bit] == bitylos[bit]) {
+			cout << "   ...sukces! ";
+			ilerazywylosowano++;
+		}
+	}
+	srednia = ilerazywylosowano / 10.0;
+	cout << "\n\nIlość losowań na sukces: " << srednia;
+	system("pause");
+	return 0;
+}
